bd37564aa4bcb5523d7206736c33105f31a298fc
yypgzyx <yypgzyx@163.com>
2020-11-09 22:02:02 +0800

    创建分支

--- HelloGit/src/main/java/com/aiguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/aiguigu/git/HelloGit.java
@@ -4,7 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("更新1");
-        System.out.println("更新2");
-        System.out.println("更新3");
+        System.out.println("创建分支");
     }
 }
